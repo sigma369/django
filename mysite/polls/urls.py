583129ade95569ad40ca8b79294e42c7923ad492
siddhesh <siddheshmane369@gmail.com>
2024-02-19 09:08:15 +0530

    tutorial 4 Class view approach

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,9 +4,10 @@ from . import views
 app_name = 'polls'
 
 urlpatterns = [
-    path("", views.index, name="polls-index"),
+    path("", views.IndexView.as_view(), name="polls-index"),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+
     path('owner', views.owner, name='owner'),
-    path('<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,28 +1,50 @@
 # from django.shortcuts import render
 
 # Create your views here.
+from typing import Any
+from django.db.models.query import QuerySet
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
+from django.views.generic import ListView, DetailView
 
 from .models import Question, Choice
 
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    question_qury_set = Question.objects.order_by('pub_date')[0:5]
-    #question_list = [q.question_text for q in question_object]
+    def get_queryset(self):
+        return Question.objects.order_by('pub_date')[0:5]
+
+
+# def index(request):
+#     question_qury_set = Question.objects.order_by('pub_date')[0:5]
+#     #question_list = [q.question_text for q in question_object]
     
-    context = {'latest_question_list': question_qury_set}
+#     context = {'latest_question_list': question_qury_set}
 
-    return  render(request, 'polls/index.html', context)
+#     return  render(request, 'polls/index.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
-    context = {'question': question,
-               'choices': Choice.objects.filter(question=question)
-               }
-    return render(request, 'polls/detail.html', context)
+class DetailView(DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+         context = super().get_context_data(**kwargs)
+         context['choices'] = Choice.objects.filter(question=context['question'])
+         print(context)
+         return context
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, id=question_id)
+#     context = {'question': question,
+#                'choices': Choice.objects.filter(question=question)
+#                }
+#     return render(request, 'polls/detail.html', context)
+    
+    
     # try:
     #     #question = Question.objects.filter(id=question_id).first()
     #     question = Question.objects.get(id=question_id)
